a756571d79ee198289e110a0d2b48bdabbe0809f
lefterissalonidis <lefsalophotography@gmail.com>
2025-06-24 21:52:08 +0300

    Add tour files

---
+++ app-files/data.js
@@ -0,0 +1,191 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2990,
+      "initialViewParameters": {
+        "yaw": 0.6999482443867588,
+        "pitch": 0.010306181176089169,
+        "fov": 1.5706620112355847
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4009290807125279,
+          "pitch": 0.5327175367469827,
+          "rotation": 0,
+          "target": "1-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hallway",
+      "name": "Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2990.5,
+      "initialViewParameters": {
+        "yaw": 1.0836993613093142,
+        "pitch": -0.019372062803078904,
+        "fov": 1.5706620112355847
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6142340505781565,
+          "pitch": 0.47907752982333207,
+          "rotation": 0,
+          "target": "3-bedroom"
+        },
+        {
+          "yaw": -1.2995204461126235,
+          "pitch": 0.5560793947339207,
+          "rotation": 0,
+          "target": "0-living-room"
+        },
+        {
+          "yaw": 0.3164719849119173,
+          "pitch": 0.6614720843277837,
+          "rotation": 0,
+          "target": "2-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2990.5,
+      "initialViewParameters": {
+        "yaw": -0.14121300311590623,
+        "pitch": 0.0020612362352174785,
+        "fov": 1.5706620112355847
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5311546322628313,
+          "pitch": 0.697468960664267,
+          "rotation": 0,
+          "target": "1-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bedroom",
+      "name": "Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2990.5,
+      "initialViewParameters": {
+        "yaw": -2.522696316860415,
+        "pitch": 0.02591311373905114,
+        "fov": 1.5706620112355847
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6055815150309627,
+          "pitch": 0.5088823107245819,
+          "rotation": 0,
+          "target": "1-hallway"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
